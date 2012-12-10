769abb09ade63cc3182298f1dfae34567e6a460e
Oskar Bley <oskar@programming-php.net>
2012-12-10 15:00:22 +0100

    error getting the flashMessenger view helper

--- config/view_helper.config.php
+++ config/view_helper.config.php
@@ -12,7 +12,9 @@ use DkplusBase\View\Helper as Helper;
 return array(
     'factories' => array(
         'flashMessenger' => function (HelperPluginManager $manager) {
-            $controllerPlugin = $manager->getServiceLocator()->get('Zend\Mvc\Controller\Plugin\FlashMessenger');
+            $controllerPlugin = $manager->getServiceLocator()
+                                        ->get('Zend\Mvc\Controller\PluginManager')
+                                        ->get('Zend\Mvc\Controller\Plugin\FlashMessenger');
             return new Helper\FlashMessenger($controllerPlugin);
         }
     )
